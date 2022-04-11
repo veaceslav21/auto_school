b7210e2c64e375c3fca0364a41b9eed3f7999ea1
Veaceslav Lefter <lefterslavic@gmail.com>
2022-04-11 16:23:40 +0300

    add lesson model without migration

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 
+class Lesson(models.Model):
+    PLACE = models.Choices('Oras', 'Poligon')
+    CATEGORY = [
+        ('A', 'A'),
+        ('A1', 'A1'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D')
+    ]
+
+    student = models.ForeignKey('account.Account', on_delete=models.CASCADE)
+    category = models.CharField(max_length=2, choices=CATEGORY)
+    place = models.CharField(max_length=10, choices=PLACE, null=False)
+    date_time = models.DateTimeField(null=False)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
