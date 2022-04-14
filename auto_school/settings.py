d2e118b31f2ad82617d855673a3d53eaf58cba64
Veaceslav Lefter <lefterslavic@gmail.com>
2022-04-14 13:27:48 +0300

    add rest Log in for browsable API

--- auto_school/settings.py
+++ auto_school/settings.py
@@ -134,6 +134,8 @@ AUTH_USER_MODEL = 'account.Account'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
 SIMPLE_JWT = {
